8da3bbe6257888dd226bd03441fce06f516ba68a
Thomas Sauerer <tsauerer@tsauerer-a01.vmware.com>
2020-08-19 12:53:50 +0200

    commit

---
+++ Gateway-Main/main.tf
@@ -0,0 +1,113 @@
+provider "nsxt" {
+  host                 = var.host
+  vmc_token            = var.vmc_token
+  allow_unverified_ssl = true
+  enforcement_point    = "vmc-enforcementpoint"
+}
+########### configure the Management Gateway and the Computegateway ###########
+/*
+You have to do a terraform import first:
+terraform import nsxt_policy_gateway_policy.cgw_policy cgw/default
+terraform import nsxt_policy_gateway_policy.mgw_policy mgw/default
+*/
+
+########### prevent destroy ###########
+/*
+please take care if you working with the management and compute gateway!
+You can destroy your whole SDDC with it!
+Lifecycle prevent you to run the destroy command and kill your Gateways.
+*/
+
+lifecycle {
+  prevent_destroy = true
+}
+
+resource "nsxt_policy_gateway_policy" "mgw_policy" {
+  category     = "LocalGatewayRules"
+  display_name = "default"
+  domain       = "mgw"
+    rule {
+        action                = "ALLOW"
+        destination_groups    = [
+            "/infra/domains/mgw/groups/VCENTER",
+        ]
+        destinations_excluded = false
+        direction             = "IN_OUT"
+        disabled              = false
+        display_name          = "vCenter Inbound Rule"
+        ip_version            = "IPV4_IPV6"
+        logged                = false
+        profiles              = []
+        scope                 = [
+            "/infra/labels/mgw",
+        ]
+        services              = ["/infra/services/HTTPS",
+                                 "/infra/services/ICMP-ALL",
+        ]
+        source_groups         = []
+        sources_excluded      = false
+    }
+
+    rule {
+        action                = "ALLOW"
+        destination_groups    = []
+        destinations_excluded = false
+        direction             = "IN_OUT"
+        disabled              = false
+        display_name          = "vCenter Outbound Rule"
+        ip_version            = "IPV4_IPV6"
+        logged                = false
+        profiles              = []
+        scope                 = [
+            "/infra/labels/mgw",
+        ]
+        services              = []
+        source_groups         = [
+            "/infra/domains/mgw/groups/VCENTER",
+        ]
+        sources_excluded      = false
+    }
+    rule {
+        action                = "ALLOW"
+        destination_groups    = []
+        destinations_excluded = false
+        direction             = "IN_OUT"
+        disabled              = false
+        display_name          = "ESXi Outbound Rule"
+        ip_version            = "IPV4_IPV6"
+        logged                = false
+        profiles              = []
+        scope                 = [
+            "/infra/labels/mgw",
+        ]
+        services              = []
+        source_groups         = [
+            "/infra/domains/mgw/groups/ESXI",
+        ]
+        sources_excluded      = false
+    }
+}
+
+resource "nsxt_policy_gateway_policy" "cgw_policy" {
+  category     = "LocalGatewayRules"
+  display_name = "default"
+  domain       = "cgw"
+
+    rule {
+        action                = "DROP"
+        destination_groups    = []
+        destinations_excluded = false
+        direction             = "IN_OUT"
+        disabled              = false
+        display_name          = "Default VTI Rule"
+        ip_version            = "IPV4_IPV6"
+        logged                = false
+        profiles              = []
+        scope                 = [
+            "/infra/labels/cgw-vpn",
+        ]
+        services              = []
+        source_groups         = []
+        sources_excluded      = false
+    }
+}
