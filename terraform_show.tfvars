dc3f0e0d1e97ba0b341a6f95933b9b18271126d4
xfirestyle2k <firestyle2k@hotmail.com>
2021-08-16 13:12:48 +0200

    commit

--- terraform_show.tfvars
+++ terraform_show.tfvars
@@ -1,2 +1,10 @@
 vmc_token = "nVe1k9vG"
 host = "nsx-XXX-XXX-XXX-Xaf66"
+
+
+OR
+
+host        = "192.168.110.41"
+username    = "admin"
+password    = "default"
+
---
+++ vars_localNSX.tf
@@ -0,0 +1,13 @@
+
+variable "host" {
+    description = "Local NSX-T Manager"
+}
+
+variable "username" {
+    description = "User Account"
+}
+
+
+variable "password" {
+    description = "User Account Password"
+}
